21010b029a54f3a3d58ce70e3e99a24e79cb941c
Kirill Brazhnikov <kirill.brazhnikov1@gmail.com>
2017-08-02 17:30:19 +0300

    1) Rename hello.c to gpio_test.c 2) Copy macro definitions and headers from gpio_test.c to gpio_raspberry_pi3.h 3) Remove unused functions from gpio_test.c 4) Change hello.o to gpio_test.o in Makefile

---
+++ test/gpio_test.c
@@ -0,0 +1,39 @@
+#include "gpio_raspberry_pi3.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+
+/****************************************************************************/
+/* Module init / cleanup block.                                             */
+/****************************************************************************/
+int gpio_init(void) {
+	int ret = 0;
+
+	printk(KERN_NOTICE "Hello !\n");
+
+	/* Allocate GPIO 17 before use */
+	if(gpio_request_one(GPIO_RPI3(17), GPIOF_INIT_HIGH, GPIO_ANY_GPIO_DESC)) {
+		printk("GPIO request faiure: %s\n", GPIO_ANY_GPIO_DESC);
+		return -ENODEV;
+	}
+
+	/* Get GPIO 17 value */
+	ret = gpio_get_value(GPIO_RPI3(17));
+	printk(KERN_NOTICE "gpio_get_value = %d\n", ret);
+
+	return 0;
+}
+
+void gpio_cleanup(void) {
+	printk(KERN_NOTICE "Goodbye\n");
+
+	/* Free GPIO 17 */
+	gpio_free(GPIO_RPI3(17));
+
+	return;
+}
+
+module_init(gpio_init);
+module_exit(gpio_cleanup);
+
